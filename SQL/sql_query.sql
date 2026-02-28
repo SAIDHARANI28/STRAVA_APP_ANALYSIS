d8d332081a8af128b3071eff8e8f5893e1cdbd79
SAI DHARANI K <ksaidharani28@gmail.com>
2026-03-01 02:17:37 +0530

    loading of tables 1,2,4 is done

---
+++ SQL/sql_query.sql
@@ -0,0 +1,12 @@
+# creating a database
+create database DA_Project2;
+use DA_Project2;
+SET GLOBAL local_infile = 1;
+SHOW VARIABLES LIKE 'local_infile';
+SHOW VARIABLES LIKE 'secure_file_priv';
+
+#creating tables uncleaned csv files
+
+
+
+
